438bd3a2d7c7a5362db80efe7f651970ed91b5d7
MehranMr17 <mrs7mehrann@gmail.com>
2025-08-11 20:21:41 +0330

    create project

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://en-mirror.ir")
+    }
+}
+
+rootProject.name = "Assistant"
+include(":app")
+ 
